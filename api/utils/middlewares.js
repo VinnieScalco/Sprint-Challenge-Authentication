70e3e271054e95a26faee18cc9b856195925532e
Vinnie <vinnie.scalco@gmail.com>
2018-01-19 11:44:07 -0600

    Utils

--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -21,15 +21,49 @@ const authenticate = (req, res, next) => {
 };
 
 const encryptUserPW = (req, res, next) => {
-  const { username, password } = req.body;
+
+  bcrypt.genSalt(SaltRounds, (err, salt) => {
+    if (err) {
+      return res.status(422).json(err);
+    }
+    bcrypt.hash(req.body.password, salt, (error, hash) => {
+      if (error) {
+        return res.status(422).json(error);
+      }
+
+      req.passwordHash = hash
+      next();
+    });
+  });
+
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // Once the password is encrypted using bcrypt you'll need to set a user obj on req.user with the encrypted PW
-  // Once the user is set, call next and head back into the userController to save it to the DB
+  // Once the password is encrypted using bcrypt, you'll need to save the user the DB.
+  // Once the user is set, take the savedUser and set the returned document from Mongo on req.user
+  // call next to head back into the route handler for encryptUserPW
 };
 
-const compareUserPW = (req, res, next) => {
-  const { username, password } = req.body;
+const compareUserPW = async (req, res, next) => {
+  try {
+
+    const { username, password } = req.body;
+    const zeUser = await User.findOne({ username });
+
+    bcrypt.compare(password, zeUser.passwordHash, (err, check) => {
+      if(err) {
+        console.log("ERR HERE");
+        return res.status(422).json(err);
+      }
+
+      req.username = zeUser.username
+      next();
+    });
+
+  } catch (e) {
+    console.log("ERROR HERE: ", e);
+    return res.status(422).json(e);
+  }
+
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // TODO: Fill this middleware in with the Proper password comparing, bcrypt.compare()
   // You'll need to find the user in your DB
